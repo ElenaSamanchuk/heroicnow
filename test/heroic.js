894c369d2aae6e4f8d0bdebebc1948a6a282dccd
ElenaSamanchuk <elenasamanchuk@gmail.com>
2018-08-16 14:35:59 +0300

    Init

--- test/heroic.js
+++ test/heroic.js
@@ -1,29 +1,41 @@
 var assert = require('assert');
 
 describe('register form', function() {
-    it('should not be messages on the screen, input of correct values', function () {
+    
+    it('input of correct values', function () {
         browser.url('https://heroicnow.com/step1.php?id=1&site=46');
         browser.setValue('input[name="firstname"]','Ivan');
         browser.setValue('input[name="lastname"]','Ivanov');
         browser.setValue('input[name="email"]','ivanovivan01011901@mail.ru');
         browser.setValue('input[name="password"]','01011901');
         browser.click('label');
-        browser.click('#submit');
-        browser.saveScreenshot('1.png');
+        browser.click('#submit')
+        assert.equal('','','should not be messages on the screen',(browser.saveScreenshot('1.png'))); 
     });   
 
-    it('should be messages on the screen, input without a tick', function () {
+    it('input of English letters', function () {
         browser.url('https://heroicnow.com/step1.php?id=1&site=46');
         browser.setValue('input[name="firstname"]','Ivan');
         browser.setValue('input[name="lastname"]','Ivanov');
         browser.setValue('input[name="email"]','ivanovivan01011901@mail.ru');
-        browser.setValue('input[name="password"]','01011901');
+        browser.setValue('input[name="password"]','abcdefgh');
+        browser.click('label');
         browser.click('#submit');
-        browser.saveScreenshot('2.png');
+        assert.equal('','','should not be messages on the screen',(browser.saveScreenshot('2.png')));
     });
 
+    it('input of characters more than 30 in the firstname and lastname, than 24 in the password', function () {
+        browser.url('https://heroicnow.com/step1.php?id=1&site=46');
+        browser.setValue('input[name="firstname"]','0123456789012345678901234567890123456789');
+        browser.setValue('input[name="lastname"]','0123456789012345678901234567890123456789');
+        browser.setValue('input[name="email"]','ivanovivan01011901@mail.ru');
+        browser.setValue('input[name="password"]','0123456789012345678901234567890123456789');
+        browser.click('label');
+        browser.click('#submit');
+        assert.equal('.error-message','.error-message','should not be messages on the screen',(browser.saveScreenshot('3.png')));
+    });
 
-    it('should be messages on the screen, input of null values', function () {
+    it('input of null values', function () {
         browser.url('https://heroicnow.com/step1.php?id=1&site=46');
         browser.setValue('input[name="firstname"]','');
         browser.setValue('input[name="lastname"]','');
@@ -31,90 +43,64 @@ describe('register form', function() {
         browser.setValue('input[name="password"]','');
         browser.click('label');
         browser.click('#submit');
-        browser.saveScreenshot('3.png');
+        assert.equal('.error-message','.error-message','should be messages on the screen',(browser.saveScreenshot('4.png'))); 
     }); 
 
-
-    it('should be messages on the screen, input of spaces in the firstname and lastname fields', function () {
+    it('input without a tick', function () {
         browser.url('https://heroicnow.com/step1.php?id=1&site=46');
-        browser.setValue('input[name="firstname"]',' ');
-        browser.setValue('input[name="lastname"]',' ');
+        browser.setValue('input[name="firstname"]','Ivan');
+        browser.setValue('input[name="lastname"]','Ivanov');
+        browser.setValue('input[name="email"]','ivanovivan01011901@mail.ru');
+        browser.setValue('input[name="password"]','01011901');
+        browser.click('#submit');
+        assert.equal('.error-message','.error-message','should be messages on the screen',(browser.saveScreenshot('12.png'))); 
+    });
+
+    it('input of spaces in the firstname and lastname fields', function () {
+        browser.url('https://heroicnow.com/step1.php?id=1&site=46');
+        browser.setValue('input[name="firstname"]','  ');
+        browser.setValue('input[name="lastname"]','  ');
         browser.setValue('input[name="email"]','ivanovivan01011901@mail.ru');
         browser.setValue('input[name="password"]','12345678');
         browser.click('label');
         browser.click('#submit');
-        browser.saveScreenshot('4.png');
-    }); 
+        assert.equal('','.error-message','should be messages on the screen',(browser.saveScreenshot('5.png'))); 
+    });
 
-    it('should be messages on the screen, input of digits in the firstname and lastname fields', function () {
+    it('input of digits in the firstname and lastname fields', function () {
         browser.url('https://heroicnow.com/step1.php?id=1&site=46');
-        browser.setValue('input[name="firstname"]','1');
-        browser.setValue('input[name="lastname"]','1');
+        browser.setValue('input[name="firstname"]','12345');
+        browser.setValue('input[name="lastname"]','12345');
         browser.setValue('input[name="email"]','ivanovivan01011901@mail.ru');
         browser.setValue('input[name="password"]','12345678');
         browser.click('label');
         browser.click('#submit');
-        browser.saveScreenshot('5.png');
-    }); 
+        assert.equal('','.error-message','should be messages on the screen',(browser.saveScreenshot('6.png')));
+   });
 
-    it('should be messages on the screen, input of Russian letters', function () {
+    it('input of characters', function () {
         browser.url('https://heroicnow.com/step1.php?id=1&site=46');
-        browser.setValue('input[name="firstname"]','а');
-        browser.setValue('input[name="lastname"]','а');
-        browser.setValue('input[name="email"]','а');
-        browser.setValue('input[name="password"]','а');
+        browser.setValue('input[name="firstname"]','/////');
+        browser.setValue('input[name="lastname"]','//////');
+        browser.setValue('input[name="email"]','ivanovivan01011901@mail.ru');
+        browser.setValue('input[name="password"]','/////////');
         browser.click('label');
         browser.click('#submit');
-        browser.saveScreenshot('6.png');
+        assert.equal('','.error-message','should be messages on the screen',(browser.saveScreenshot('7.png')));
     });
 
-
-    it('should be messages on the screen, input of English letters', function () {
+    it('input one character in the firstname and lastname', function () {
         browser.url('https://heroicnow.com/step1.php?id=1&site=46');
-        browser.setValue('input[name="firstname"]','b');
-        browser.setValue('input[name="lastname"]','b');
-        browser.setValue('input[name="email"]','b');
-        browser.setValue('input[name="password"]','b');
+        browser.setValue('input[name="firstname"]','a');
+        browser.setValue('input[name="lastname"]','a');
+        browser.setValue('input[name="email"]','ivanovivan01011901@mail.ru');
+        browser.setValue('input[name="password"]','123456');
         browser.click('label');
         browser.click('#submit');
-        browser.saveScreenshot('7.png');
+        assert.equal('','.error-message','should be messages on the screen',(browser.saveScreenshot('8.png')));
     });
 
-
-    it('should be messages on the screen, input of characters', function () {
-        browser.url('https://heroicnow.com/step1.php?id=1&site=46');
-        browser.setValue('input[name="firstname"]','/');
-        browser.setValue('input[name="lastname"]','/');
-        browser.setValue('input[name="email"]','/');
-        browser.setValue('input[name="password"]','/');
-        browser.click('label');
-        browser.click('#submit');
-        browser.saveScreenshot('8.png');
-    });
-
-    it('should be messages on the screen, input of characters more than 30 in the firstname and lastname, than 24 in the password', function () {
-        browser.url('https://heroicnow.com/step1.php?id=1&site=46');
-        browser.setValue('input[name="firstname"]','0123456789012345678901234567890123456789');
-        browser.setValue('input[name="lastname"]','0123456789012345678901234567890123456789');
-        browser.setValue('input[name="email"]','0123456789012345678901234567890123456789');
-        browser.setValue('input[name="password"]','0123456789012345678901234567890123456789');
-        browser.click('label');
-        browser.click('#submit');
-        browser.saveScreenshot('9.png');
-    });
-
-    it('should be messages on the screen, input of 4 characters in the password', function () {
-        browser.url('https://heroicnow.com/step1.php?id=1&site=46');
-        browser.setValue('input[name="firstname"]','0123');
-        browser.setValue('input[name="lastname"]','0123');
-        browser.setValue('input[name="email"]','0123');
-        browser.setValue('input[name="password"]','0123');
-        browser.click('label');
-        browser.click('#submit');
-        browser.saveScreenshot('10.png');
-    });
-
-    it('should be messages on the screen, input adress without .ru', function () {
+    it('input adress without .ru', function () {
         browser.url('https://heroicnow.com/step1.php?id=1&site=46');
         browser.setValue('input[name="firstname"]','Ivan');
         browser.setValue('input[name="lastname"]','Ivanov');
@@ -122,12 +108,30 @@ describe('register form', function() {
         browser.setValue('input[name="password"]','01011901');
         browser.click('label');
         browser.click('#submit');
-        browser.saveScreenshot('11.png');
+        assert.equal('','.error-message','should be messages on the screen',(browser.saveScreenshot('9.png')));
     });
 
-
-
-
-
-
+    it('input of Russian letters in the password', function () {
+        browser.url('https://heroicnow.com/step1.php?id=1&site=46');
+        browser.setValue('input[name="firstname"]','Иван');
+        browser.setValue('input[name="lastname"]','Иванов');
+        browser.setValue('input[name="email"]','ivanovivan01011901@mail');
+        browser.setValue('input[name="password"]','абвгдеж');
+        browser.click('label');
+        browser.click('#submit');
+        assert.equal('','.error-message','should be messages on the screen',(browser.saveScreenshot('10.png')));
     });
+
+    it('input of 4 characters in the password', function () {
+        browser.url('https://heroicnow.com/step1.php?id=1&site=46');
+        browser.setValue('input[name="firstname"]','Ivan');
+        browser.setValue('input[name="lastname"]','Ivanov');
+        browser.setValue('input[name="email"]','ivanovivan01011901@mail');
+        browser.setValue('input[name="password"]','1234');
+        browser.click('label');
+        browser.click('#submit');
+        assert.equal('','.error-message','should be messages on the screen',(browser.saveScreenshot('11.png')));
+    });
+    });  
+
+       
